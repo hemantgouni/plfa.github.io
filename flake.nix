b6700e732ce7158db76f91fc1e9f9ba659488759
Hemant Gouni <hemant@hemantgouni.com>
2021-08-17 16:28:42 +0000

    Package plfa with Nix

--- flake.nix
+++ flake.nix
@@ -1,18 +1,37 @@
 {
+  inputs.nixpkgs.url = "github:nixos/nixpkgs?rev=860b56be91fb874d48e23a950815969a7b832fbc";
   inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.plfa.url = "github:plfa/plfa.github.io";
+  inputs.plfa.flake = false;
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, flake-utils, plfa }:
     flake-utils.lib.eachDefaultSystem
     (system:
-    let pkgs = nixpkgs.legacyPackages.${system}; in {
+    let
+      pkgs = nixpkgs.legacyPackages.${system};
+      agda = pkgs.agda.withPackages (p: [
+        p.standard-library
+        (p.mkDerivation {
+          pname = "plfa";
+          meta = null;
+          version = "1.0.0";
+          buildInputs = [ p.standard-library ];
+          preBuild = ''
+            echo "module Everything where" > Everything.agda
+			find src -name '*.lagda.md' | sed -e 's/src\///' -e 's/\.lagda\.md//' -e 's/\//\./g' -e 's/^/import /' | grep -Ev '^import plfa\.part1\.Equality|Naturals$' >> Everything.agda
+            export LANG=C.UTF-8
+          '';
+          src = plfa;
+        })
+      ]);
+    in {
       devShell =
         pkgs.mkShell {
-          buildInputs = 
-          with pkgs; [
-            emacs
+          buildInputs = [
+            pkgs.emacs
             agda
           ];
         };
-    });
-}
+      });
+    }
 
